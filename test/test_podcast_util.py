c66acca6a4d060f41143af6b496baa36b9d57c2a
blue <neelabalan.n@outlook.com>
2021-02-02 20:09:28 +0530

    more test coverage

--- test/test_podcast_util.py
+++ test/test_podcast_util.py
@@ -91,3 +91,31 @@ def test_construct_episodes_message(pu, feed):
     ]
     assert pu._construct_episodes_message(feed) == expected
 
+@mock.patch('podcast.podcast_util.PodcastUtil.get_feeds')
+def test_get_dict(mock_get_feeds, pu, feed):
+    mock_get_feeds.return_value = feed
+    expected_dict = {
+        'PodcastTitle': {
+            'etag': '5f77c6d7-45f1e',
+            'last_updated': str(datetime.datetime(2021, 1, 1)),
+            'rsslink': 'https://sample.podcast.tv/test.xml',
+            'link': 'https://sample.com',
+            'subtitle': 'This is the best podcast show in the entire universe',
+            'episodes': [
+                {
+                    'title': 'Episode1',
+                    'subtitle': 'episode of week 1',
+                    'published': str(datetime.datetime(2021, 1, 1)),
+                    'audiolink': 'https://somesite.com/episode1.mp3',
+                    'link': 'https://somesite.com/episode1',
+                },
+                {
+                    'title': 'Episode2',
+                    'subtitle': 'episode of week 2',
+                    'published': str(datetime.datetime(2021, 1, 7)),
+                    'audiolink': 'https://somesite.com/episode2.mp3',
+                    'link': 'https://somesite.com/episode2',
+                }
+            ]
+        }
+    }
